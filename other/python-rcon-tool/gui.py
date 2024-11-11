df88d464ed0c7521ad395696dfc2391a19ba7336
Sandor Maro <marosandor2001@gmail.com>
2024-11-11 23:04:13 +0100

    rcon-tool created

---
+++ other/python-rcon-tool/gui.py
@@ -0,0 +1,13 @@
+import sys
+from PyQt5 import QtWidgets, uic
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        uic.loadUi('rcon_gui.ui', self)  # A 'design.ui' helyére írja be a saját fájlnevét
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
